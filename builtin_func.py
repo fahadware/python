65963656a4c8b1fda78461d882d38d08c3066d94
Fahad <fahadkk833@gmail.com>
2026-07-05 12:39:45 +0500

    new lines

--- builtin_func.py
+++ builtin_func.py
@@ -10,3 +10,9 @@ print(name.lower())
 print(name.upper())
 print(name.capitalize()) #convert 1st letter of string into capital..
 print(name.title())      #convert 1st letter of each word..
+
+#cleaning
+word=" hello world "
+print(word.strip())
+print(word.lstrip())
+print(word.rstrip())
