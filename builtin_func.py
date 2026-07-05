55bd578796d121846c7385b956150f29ca0d984e
Fahad <fahadkk833@gmail.com>
2026-07-05 13:12:58 +0500

    new lines

--- builtin_func.py
+++ builtin_func.py
@@ -16,6 +16,10 @@ word="hello world"
 print(word.strip())
 print(word.lstrip())
 print(word.rstrip())
+
+user_input = "   FAHAD.ALI@EMAIL.COM   "
+cleaned_email = user_input.strip().lower()
+print(cleaned_email)
 #searching
 print(word.find("l"))
 print(word.count("l"))
@@ -28,3 +32,30 @@ print(word.join("-"))
 #replace
 newword="cat"
 print(newword.replace("c","b"))
+#IN FUNTION....
+email = "fAHAD.ALI@example.com"
+if "@" in email:
+    print("Valid email format")
+else:
+    print("Invalid email")
+
+    #LETS BUILD SIMPLE data CLEANER....
+
+raw_data=input("Enter some sentence of data:")
+email=input("Enter an email of source:")
+if "@" and email.lower()==True in email:
+    print("Email is valid")
+else:
+    print("Enter Valid email plz...")
+
+cleaned_data=raw_data.strip().lower()
+word_count=len(cleaned_data.split())
+upercase_version=cleaned_data.upper()
+find_word=cleaned_data.find("f")
+
+print("----Analysis---------")
+print("Entered text is : ",raw_data)
+print()
+print("Cleaned Data  : ",cleaned_data)
+print("Uppercase version:", upercase_version)
+print("Mentions How many words:", find_word)
