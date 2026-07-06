599cc080a42a7029fce6c088e2b6317b830aebb5
Fahad <fahadkk833@gmail.com>
2026-07-06 21:39:16 +0500

    new lines of code addes

--- tuples.py
+++ tuples.py
@@ -3,4 +3,19 @@ names=("Fahad","Hammad","Shahzaib")
 for i in names:
     print(i)
 
-print(names[0])
+print(names[0])  #Accessing items
+
+coordinates = (10, 20)
+
+print(coordinates[0])    # 10
+print(coordinates[-1])   # 20
+
+coordinates[0] = 99   #  TypeError! because tuples are unchangable...
+
+
+std_info=("Fahad","21","CS","3.1")
+name,age,department,cgpa=std_info  #Tuple unpacking
+print(name)
+print(age)
+print(department)
+print(cgpa)
