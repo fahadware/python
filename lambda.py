4d60b3f06772bbf92ccc760d1aae14667770394a
Fahad <fahadkk833@gmail.com>
2026-07-11 20:33:35 +0500

    new concept

--- lambda.py
+++ lambda.py
@@ -25,7 +25,7 @@ print(sorted_std)
 
 numbers=[1,2,3,4,5,6,8,9,10,11,12]
 
-even=list(filter(lambda x:x%2==0,numbers))
+even=list(filter(lambda x:x%2==0,numbers))  #filter() keeps only the items where the lambda returns True..
 print(even)
 
 #map function
@@ -33,3 +33,12 @@ numbers=[1,2,3,4,5]
 square=list(map(lambda x:x*x,numbers))  #map will apply lamda fucntion to all numbers each time
 print(square)
 
+
+# Forgetting key= when using lambda with sorted()/max()/min()
+students = [("Ali", 85), ("Sara", 92)]
+#    sorted_students = sorted(students, lambda s: s[1])   # ❌ TypeError — missing "key="
+sorted_students = sorted(students, key=lambda s: s[1])   # ✅ correct
+
+numbers=[20,10,30,100,35,15]
+maximum=max(numbers,key=lambda x:x)
+print(maximum)
