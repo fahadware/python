f1a96c609bd142a1cceaa4e1dcd7dfa2e56a2eaf
Fahad <fahadkk833@gmail.com>
2026-07-13 20:29:16 +0500

    New file added named file_handling.py

---
+++ file_handling.py
@@ -0,0 +1,33 @@
+#open file and write an statment by "with" statment
+with open("My_file.txt","w") as myfile: #w->write
+    myfile.write("Hello from fahad")
+
+with open("My_file.txt","r") as myfile:  #r->read(file myst be exist)
+    content=myfile.read()
+    print(content)
+
+with open("My_file.txt","r") as myfile:
+    for lines in myfile:
+        print(lines)
+
+with open("My_file.txt","a") as myfile:
+    myfile.write("\nI am computer science student")
+#after appending text....
+
+with open("My_file.txt","r") as myfile:  #r->read(file myst be exist)
+    content=myfile.read()
+    print(content)
+
+with open("My_file.txt", "r") as file:
+     content = file.read()
+     file.close()   # you must remember to do this manually!
+     print(content)
+
+lines_to_write=["Fahad\n","Hammad\n","Shahzaib\n"]
+with open("Names.txt","w") as newfile:
+    newfile.writelines(lines_to_write)  #write =lines is used fro writing multiple lines at once
+
+with open("Names.txt","r") as myfile:
+    content=myfile.read()
+    print(content)
+
