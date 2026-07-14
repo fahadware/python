7b3fa215e5d3b1f64b57615c554c60d8939c7832
Fahad <fahadkk833@gmail.com>
2026-07-15 00:39:28 +0500

    new

--- oop.py
+++ oop.py
@@ -57,4 +57,71 @@ print(my_animal.name)
 print(my_animal.breed)
 my_animal.animal_sound()
 
+#modifing clas attribute
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"{self.owner}'s new balance: {self.balance}")
+
+account = BankAccount("Ali", 1000)
+account.deposit(500)
+
+#inheritence
+class Animal:
+    def __init__(self,name):
+        self.name=name
+    def eat(self):
+        print(self.name,"Is eating")
+class Dog(Animal):
+    def bark(self):
+        print(self.name,"Says wooo!")
+
+mydog=Dog("puppy")
+mydog.eat()
+mydog.bark()
         
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def make_sound(self):
+        print(f"{self.name} says {self.sound}")
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Woof")   # reuse Animal's setup logic
+
+my_dog = Dog("Puppy")
+my_dog.make_sound()
+
+#Polymorphism — same method name, different behavior
+class cat:
+    def speak(self):
+        print("Meow")
+class dog:
+    def speak(self):
+        print("wooo")
+Animals=[cat(),dog()]
+for animals in Animals:
+    animals.speak()
+        
+#encapsulation
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self._balance = balance   # single underscore = "internal use" convention->private
+
+    def withdraw(self, amount):
+        if amount > self._balance:
+            print("Insufficient funds")
+        else:
+            self._balance -= amount
+            print(f"Withdrew {amount}. New balance: {self._balance}")
+
+account = BankAccount("Ali", 1000)
+account.withdraw(200)
